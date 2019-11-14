f2fc6aff633db9e7c4265df73edafdd064edc2ce
geobde <geobde@gmail.com>
2019-11-14 04:13:49 +0200

    First commit

---
+++ src/useTruncate.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react';
+
+const defaultSettings = {
+   length:30,
+   ending:'...'
+};
+
+export const useTextTruncate = (str, settings = defaultSettings) => {
+  const [text, setText] = useState(str);
+
+  useEffect(() => {
+      if (text.length > settings.length) {
+        setText(text.substring(0, settings.length - settings.ending.length) + settings.ending);
+      } 
+  },[text]);
+
+  return {text, setText};
+
+};
